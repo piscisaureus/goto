950c43733aba5e9f73628a2fb6a164f1f0b2d785
Bert Belder <bertbelder@gmail.com>
2011-10-11 00:40:52 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+
+var express = require('express'),
+    socket_io = require('socket.io');
+
+var notes = {};
+
+
+
+var app = express.createServer();
+
+app.use(app.router);
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res){
+    res.redirect('/index.html');
+});
+
+app.listen(process.env.C9_PORT || 80);
+
+
+var io = socket_io.listen(app);
+
+io.sockets.on('connection', function(client) {
+  client.emit('init', notes);
+  
+  client.on('note', function(note, clientId) {
+    io.sockets.emit('note', note, clientId);
+    
+    if (note.message) {
+      notes[note.id] = note; 
+    } else {
+      delete notes[note.id]; 
+    }
+  });
+});
+
